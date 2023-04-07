3c0d314f49a353353a49e06db1c39667c07a08e2
carlynorama <carlynorama@gmail.com>
2023-04-06 22:01:01 -0700

    signature check

--- Sources/SwiftLIBPNG/SwiftLIBPNG.swift
+++ Sources/SwiftLIBPNG/SwiftLIBPNG.swift
@@ -9,8 +9,7 @@ import png
 
 public struct SwiftLIBPNG {
     //http://www.libpng.org/pub/png/book/chapter08.html#png.ch08.tbl.1
-    static let pngFileSignature:Data = Data([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A])
-    
+    public static let pngFileSignature:[UInt8] = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]
     
     
     public init() {
@@ -20,6 +19,29 @@ public struct SwiftLIBPNG {
         let version = png_access_version_number()
         print(version)
     }
-}
     
- 
+    public static func versionInfo()
+    {
+        let test2 = String(format:"   Compiled with libpng %@; using libpng %lu.\n", PNG_LIBPNG_VER_STRING, png_access_version_number())
+        print(test2)
+        
+    }
+    
+    public static func checkSignature(data:Data, offset:Int = 0) -> Bool {
+        withUnsafeBytes(of:data) { bytesPtr in
+            if let base = bytesPtr.assumingMemoryBound(to: UInt8.self).baseAddress {
+                return isValidSignature(bytePointer: base + offset, start: 0, count: 8)
+            } else {
+                return false
+            }
+        }
+    }
+    
+    static func isValidSignature(bytePointer:UnsafePointer<UInt8>, start:Int = 0, count:CInt) -> Bool {
+        //png_sig_cmp(T##sig: png_const_bytep!##png_const_bytep!, T##start: Int##Int, T##num_to_check: Int##Int)
+        return png_sig_cmp(bytePointer, start, 8) == 0
+        
+    }
+}
+
+
