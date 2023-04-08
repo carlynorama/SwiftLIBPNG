ed205e60782540aa0fef3e522cdea83bc8ca4db4
carlynorama <carlynorama@gmail.com>
2023-04-08 11:32:12 -0700

    for got exit routine

--- Sources/SwiftLIBPNG/SwiftLIBPNG+SimpleData.swift
+++ Sources/SwiftLIBPNG/SwiftLIBPNG+SimpleData.swift
@@ -126,6 +126,18 @@ extension SwiftLIBPNG {
             //TODO: Confirm theory has to be inside so row pointers still valid.
             png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, nil)
         }
+        
+        //--------------------------------------------------   FUNCTION CLEANUP
+        if png_ptr != nil {
+            if info_ptr != nil {
+                png_destroy_write_struct(&png_ptr, &info_ptr);
+            } else {
+                png_destroy_write_struct(&png_ptr, nil);
+            }
+        }
+        png_ptr = nil 
+        info_ptr = nil
+        //---------------------------------------------------------------------
     
 
         
