d18b73422922ede1ec3688b20e29165b3225c504
carlynorama <carlynorama@gmail.com>
2023-04-21 12:41:21 -0700

    add alittle

--- Sources/SwiftLIBPNG/Main Functions/SwiftLIBPNG+ThrowingNonPaletteTypeData.swift
+++ Sources/SwiftLIBPNG/Main Functions/SwiftLIBPNG+ThrowingNonPaletteTypeData.swift
@@ -18,6 +18,16 @@ import CBridgePNG
 
 extension SwiftLIBPNG {
     public static func pngData(for pixelData:inout [UInt8], width:UInt32, height:UInt32, bitDepth:BitDepth, colorType:ColorType) throws -> Data {
+        
+        precondition(colorType != .palletized) //cannot handle that yet.
+
+        guard let ihdr = try? IHDR(width: width, height: height, bitDepth: bitDepth, colorType: colorType) else {
+            throw PNGError("pngData:Could not make IHDR")
+        }
+        
+        
+        
+        
         return Data()
     }
 }
