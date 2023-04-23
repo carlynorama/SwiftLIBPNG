a54b4a495770fe7db0eda6f87953ca5649b6d288
carlynorama <carlynorama@gmail.com>
2023-04-23 11:13:38 -0700

    tidy, move preocondition

--- Sources/SwiftLIBPNG/Chunks/tEXt.swift
+++ Sources/SwiftLIBPNG/Chunks/tEXt.swift
@@ -16,35 +16,39 @@
 //    Warning     Warning of nature of content
 //    Source     Device used to create the image
 //    Comment     Miscellaneous comment
+//
+// http://www.libpng.org/pub/png/spec/iso/index-object.html#11textinfo
+
 
 import Foundation
 import png
 
-
-//ContiguousArray<CChar>//<- This tells you what a Swift String is not.
-// - not contiguous.
-// - not chars.
 
 extension SwiftLIBPNG {
     
     //Right now can only do English
     struct tEXt {
         let compression:ForLibPNG
-        //when these get saved as Strings can fool you into thinking its working
-        //but halfway through your string it's all like "byeeeee!" or "\uhqh389\j884fjf HAHAHA"
+
+        
+        //must be between 1 and 79 characters
         let key:ContiguousArray<CChar>
+        //TODO: if length over x chars use different compression. (zTXt)
         let value:ContiguousArray<CChar>
+        
         let text_length:Int
         let key_length:Int
         
         
         init(key:String, value: String, compression:Int32 = PNG_TEXT_COMPRESSION_NONE) {
-            precondition(key.count <= 79) //really should check C string, but fine for now.
+             //really should check C string, but fine for now.
             self.compression = compression
-            self.key = key.utf8CString
+            let CKey = key.utf8CString
+            precondition((1...79).contains(CKey.count))
+            self.key = CKey
             self.value = value.utf8CString
-            self.text_length = self.value.count //- 1 //TODO: Check this
-            self.key_length = self.key.count// - 1
+            self.text_length = self.value.count
+            self.key_length = self.key.count
             
             print(self.key)
             print(self.value)
